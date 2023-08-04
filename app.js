e4ec6813c79bfc82c115531c1355d2e7d3c84b1d
Gzus24 <jromero3418@gmail.com>
2023-08-04 09:31:57 -0700

    js file updated

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-const applyTaxBtn = document.getElementById('btn');
-const showNetPay = document.querySelector('.result');
-const fedTaxExcluded = document.querySelector('.span');
+function NetPay(){
+    const applyTaxBtn = document.getElementById('btn');
+    const showNetPay = document.querySelector('.result');
+    const fedTaxExcluded = document.querySelector('.span');
 
-class NetPay{
-    constructor(hour, payRate){
-        this.hour = hour;
-        this.payRate = payRate;
+    this.getInputVal = function(){
+        applyTaxBtn.onclick = ()=>{
+            const hours = parseInt(document.getElementById('workHr').value);
+            const hourRate = parseInt(document.getElementById('hrRate').value);
+         
+           this.getNetPay(hours, hourRate)
+        }
+      
     }
-    calculate(){
+    this.getNetPay = (hour, payRate)=>{
         let taxRate = 0.0765;
-        let grossPay = this.hour * this.payRate;
-        let taxDeduction = grossPay * taxRate;
-        if(isNaN(taxDeduction)){
-            showNetPay.textContent = "Enter a number"
-        }
-        else {
-            showNetPay.textContent =  `$ ${Math.floor(grossPay - taxDeduction)}`;
-            fedTaxExcluded.textContent = '(federal income taxes not included)';
-        }
+            let grossPay = hour * payRate;
+            let taxDeduction = grossPay * taxRate;
+                if(isNaN(taxDeduction)){
+                    showNetPay.textContent = "Enter a number"
+                }
+                else {
+                    showNetPay.textContent =  `$ ${Math.floor(grossPay - taxDeduction)}`;
+                    fedTaxExcluded.textContent = '(federal income taxes not included)';
+            
+                }
+            }
     }
-}
-
-applyTaxBtn.onclick = ()=>{
-    const hours = parseInt(document.getElementById('hour').value);
-    const hourRate = parseInt(document.getElementById('hrRate').value);
-    let employee = new NetPay(hours, hourRate);
-    employee.calculate();
-}
+    const pay = new NetPay();
+    pay.getInputVal()
+   
+         
